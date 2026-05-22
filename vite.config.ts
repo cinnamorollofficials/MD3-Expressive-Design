06068a853018d4d91f92fc2a0c398b44b195788e
hadi <jtgunawan007@gmail.com>
2026-05-22 17:57:29 +0700

    chore: setup github pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-export default defineConfig({
-  plugins: [react()],
-  server: { port: 5173, open: true },
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    base: process.env.GITHUB_ACTIONS ? '/MD3-Expressive-Design/' : '/',
+    server: { port: 5173, open: true },
+    build: {
+      outDir: 'dist-demo',
+    },
+  };
 });
